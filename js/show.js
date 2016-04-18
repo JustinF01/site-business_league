4b778ed12a1a9958bdefe2ca71b99712c4d34634
Justin Fester <justinlfester@gmail.com>
2016-04-18 20:57:26 +0200

    More refinements
    Just refined the site some more

---
+++ js/show.js
@@ -0,0 +1,7 @@
+$('.view-testimonials').on('click', function() {
+    $('.video-viewer2').fadeToggle('slow').toggleClass('show');
+});
+
+$('.view-ads').on('click', function() {
+    $('.video-viewer1').fadeToggle('slow').toggleClass('show');
+});
