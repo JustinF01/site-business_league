5b1b9d790bebd127db6c5bd21a434f3eeaa2fdbb
Justin Fester <justinlfester@gmail.com>
2016-04-25 19:31:25 +0200

    More refinements and content
    Added the last few changes to content and uploaded to server.

---
+++ index.php
@@ -0,0 +1,200 @@
+<!doctype html>
+  <html lang="en">
+    <head>
+      <meta charset="utf-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <meta name="description" content="The Business League is an organisation that is driven to create a resourceful network of professionals in order to make a change in South Africa">
+      <meta name="keywords" content="additional income, extra money opportunity, entrepreneurship, additional income, cape town, additional income cape town, entrepreneurial opportunity Cape Town">
+      <title>Creating Change</title>
+      <link rel="icon" type="image/png" href="images/favicon.png">
+      <link rel="stylesheet" href="css/style.css">
+      <link rel="stylesheet" href="css/responsive.css">
+      <link href='https://fonts.googleapis.com/css?family=Maven+Pro' rel='stylesheet' type='text/css'>
+      <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
+      <script src="js/slideScroll.js"></script>
+    </head>
+    <body>
+      <header class="main-header">
+        <div class="logo">
+          <img src="images/bl-logo.svg" alt="The Business League">
+        </div>
+        <div class="joinUp"><a href="#interestForm">Join Now!</a></div>
+        <!-- <p class="slogan">Building a network of resourceful entrepreneurs</p> -->
+        <nav>
+          <ul class="mainNav">
+           <li><a href="#strategy">Strategy</a></li>
+           <li><a href="vision.html">Vision</a></li>
+           <li><a href="faq.html">FAQs</a></li>
+          </ul>
+        </nav>
+      </header>
+      
+     
+      <section class="intro">
+         <h2>About</h2>
+  
+         <p>The Business League is an organisation designed to provide a service to help support and develop a network of innovators in South Africa.</p>
+
+         <p>The League has a very specific plan in place to support each member earn a growing monthly income and be apart of greater change in South Africa.</p>
+ 
+         <p>The league makes use of the IFA business program, offered through Clientele Life to provide willing entrepreneurs an opportunity to build a growing monthly income by refering others to join, using this website.</p>
+
+         <p>We will provide help and support every step of the way.</p>
+
+         <p>The League will achieve its goals by each member following the strategy that is set out below.</p>
+      </section>
+
+      <div class="headingbg" id="strategy">
+	     <h1>Strategy</h1>
+	    </div>
+
+      <section class="strategy clearfix">
+	       <div class="imgWrap">
+	         <img src="images/strategy-vector.svg" alt="picture of maze with a golden route through the maze">
+	       </div>
+
+         <div class="textWrap">
+  	       <p>Become a member of the Business League by completing the <a href="#interestForm">Interest form</a> at the bottom of the page and apply for an <a href="faq.html#ifa-business-acc">IFA</a> Business Account with Clientele Life.</p>
+
+           <p>Our support manager will contact you and organise a meeting to complete your application process.</p>
+
+  	       <p>On successful application, each member becomes a <a href="faq.html#stakeholder">stakeholder</a> of the organisation and is assigned a unique number (aka IFA number).</p>
+         </div>
+      </section>
+       <div class="headingbg" id="stakeholder-resposibility">
+         <h1>Stakeholder Responsibility</h1>
+       </div>
+      <section class="sr clearfix">
+          <div class="imgWrap">
+            <img src="images/stakeholder-vector.svg" alt="picture of potential investors">
+          </div>
+          
+          <div class="textWrap">
+            <p>All you are required to do as a stakeholder is to spread the word and grow membership by refering others to this website with your unique IFA number. We will do the rest.<a href="faq.html#responsibility">Why?</a></p>
+
+            <p>Our support consultant will contact each new member and organise a face to face meeting to complete your application for your <a href="faq.html#ifa-business-acc">IFA Business Account</a>. All information will be explained in person.</p>
+
+            <p>By fulfilling your responsibility and getting new members, your IFA business account will start to build a growing monthly salary directly into your bank account.</p>
+
+             <p>At the same time you will be growing a resourceful network of like minded people. Each person will earn an income through refering others.</p>
+          </div>
+       </section>
+
+       <div class="headingbg" id="member-benefits">
+	       <h1>Member Benefits</h1>
+	     </div>
+
+	     <section class="benefits clearfix">
+	       <div class="benefits-img">
+           <img src="images/benefits-vector.svg" alt="picture of a small tree growing money">
+         </div>
+
+         
+           <ul class="benefitsList">
+  	       	  <li class="firstChild b-list-item">
+  	       	     <h3>Stakeholder in Organisation</h3>
+
+  	       	     <p>Various resources and information will be shared via our facebook page with the idea of informing, motivating and inspiring innovation. Be an active participant.</p>
+                 <a href="faq.html#stakeholder">Learn More.</a>
+  	       	  </li>
+  	       	  <li class="b-list-item">
+  	       	     <h3>Networking Platform</h3>
+
+  	       	     <p>The business league will host various networking events that all members can attend for free.</p>
+                 <a href="faq.html#networking">Learn More.</a>
+  	       	  </li>
+  	       	  <li class="b-list-item">
+  	       	     <h3>IFA Business Account</h3>
+
+  	       	     <p>A means of earning an additional monthly income as well as many other benefits received through this proven system.</p>
+                 <a href="faq.html#ifa-business-acc">Learn More</a>
+  	       	  </li>
+  	       	  <li class="b-list-item">
+  	       	     <h3>A Trusted Financial Product</h3>
+
+  	       	     <p>A choice of affordable, quality insurance and investment products from Clientele Life.</p>
+                 <a href="faq.html#insurance">Learn More</a>
+  	       	  </li>
+           </ul>
+    
+	     </section>
+
+	   
+	     <section class="network clearfix">
+          <h1 id="network">Spread The Word...</h1>
+
+          <div class="alignText1">
+            <p class="center">Search through your existing networks to find like minded people.</p>
+          </div>
+
+          <div class="wrapper">
+            <div class="imgWrap">
+               <img src="images/social-icons.svg" alt="popular social networks">
+            </div>
+
+            <div class="network-layout">
+              <h1 class="left-align">Refer them here...</h1>
+
+              <p>Send them a link to this website so that they can make an informed decision.</p>
+
+              <p>The League is about building a network of game changers, so get the right people on board.</p>
+
+            </div>
+          </div>
+	     </section>
+
+	     <section class="projects" id="future-projects">
+	       <h1>Future Projects</h1>
+
+         <div class="alignText1">
+	         <p>Once membership grows the league plans on implementing various innovative projects in SA to help drive change and progress in development.</p>
+	       
+	         <p>As a member you have the opportunity to suggest projects in your community.</p>
+         </div>
+
+	       <div class="projectList">
+           <div class="number1"></div>
+	         <h4 class="one">Free WiFi in libraries.</h4>
+	         <h4 class="two">More accesable internet in SA.</h4>
+	         <h4 class="three">Training and Development in poor communities.</h4>
+	         <h4 class="four">Your Ideas.</h4>
+         </div>
+	     </section>
+
+	     <section class="interest" id="contact">
+          <h1>Interested?</h1>
+
+          <div class="alignText1">
+            <p>We need you in order to grow membership. It's simple and easy. We are here to provide help and support. Let's make a change together.</p>
+          </div>
+
+          <form action="mailscript.php" method="post" class="interestForm" id="interestForm">
+          <h2>Join Now</h2>
+          <p>Complete this simple form and our support manager will contact you to complete the application process.</p>
+          
+          <input type="text" id="name" name="name"  placeholder="Name*" required>
+          
+          <input type="email" id="email" name="email" placeholder="Email*" required>
+          
+          <input type="text" id="number" name="number" placeholder="Tel*" required>
+          
+          <input type="text" id="ifa" name="ifa" placeholder="IFA ref no: (referal only)">
+          
+          <input type="text" id="ref" name="ref" placeholder="Referer tel: (referal only)">
+
+          <input type="submit" id="submitButton" name="submitButton">
+          </form>
+
+          <a href="http://www.facebook.com/businessleagueSA" target="_blank"><img src="images/facebook-logo.svg" alt="facebook logo"></a>
+
+          <a href="mailto:justin@designbiz.co.za" class="email">justin@designbiz.co.za</a>
+          
+	     </section>
+       <footer>
+          <p>&copy;Business League SA 2016</p>
+          <p>Powered By:</p>
+          <a href="http://www.ifa.co.za" class="credit" target="_blank"><img src="images/ifa-logo.png" alt="IFA business opportunity provided by Clientele Life"></a><div class="credit"><a href="http://www.clientelelifeinsurance.co.za/" target="_blank"><img src="images/clientele-logo.png" alt="Clientele Life, A registered financial services provider"></a></div>
+          <p>background by:<a href="http://www.freepik.com/free-vector/mosaic-pattern_778327.htm" target="_blank">Freepik</a></p>
+       </footer>
+    </body>
+  </html>
--- mailscript.php
+++ mailscript.php
@@ -1,1 +1,55 @@
-$name = $_POST('');
+<?php
+
+$to='justin@designbiz.co.za';
+$subject='New Member Subscription-Business League';
+
+$name=$_POST['name'];
+$email=$_POST['email'];
+$tel=$_POST['number'];
+$ifa=$_POST['ifa'];
+$referenceNumber= $_POST['ref'];
+
+$message = <<<EMAIL
+
+Name: $name
+Email: $email
+Tel: $tel
+Referer IFA #: $ifa
+Referer tel: $referenceNumber
+
+
+EMAIL;
+mail($to, $subject, $message, "From:".$email);
+
+$thankYouContact = <<<HTML
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="keywords" content="website development services, web development, web design, graphic design, design portfolio, justin fester, design" />
+    <meta name="description" content="The exclusive web and graphic design portfolio, Brought to you by Justin Fester and his team of designers." />
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
+    <title>Justin Fester|Designer|Developer</title>
+    <link rel="icon" type="image/png" href="images/favicon.png">
+    <link href='https://fonts.googleapis.com/css?family=Actor|Raleway:400,300' rel='stylesheet' type='text/css'>
+    <link rel="stylesheet" href="css/style.css"> 
+    
+  </head>
+    <body>
+        
+		<section class="thankYou" style="max-width: 1020px; margin: 0 auto; padding: 10%;">
+            <h1>Thank you for your message.</h1>
+            <p>I will contact you soon.</p>
+            <a href="index.php">
+                <button class="thankYouButton button" style="width: 100%; padding: 3%; border: none; border-radius: 10px; font-size: 1.125em; margin-top: 5%;">Back To Site</button>
+            </a>
+        </section>
+    </body>
+</html> 
+HTML;
+
+echo $thankYouContact;
+
+
+?>
